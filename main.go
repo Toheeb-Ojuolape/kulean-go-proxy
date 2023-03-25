13db7505f397fd15451ea3a0e62cd7a430bfd0a3
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2023-03-25 12:46:26 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func ReverseProxyAPIGet(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Query().Get("url")
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", r.Header.Get("Authorization"))
+	req.Header.Set("Channel", "Web")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
+}
+
+func ReverseProxyAPILogin(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Query().Get("url")
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", url, r.Body)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	req.Header.Set("Content-Type", r.Header.Get("Content-Type"))
+	req.Header.Set("Authorization", r.Header.Get("Channel"))
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
+}
+
+func ReverseProxyAPIPost(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Query().Get("url")
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", url, r.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	req.Header.Set("Content-Type", r.Header.Get("Content-Type"))
+	req.Header.Set("Authorization", r.Header.Get("Authorization"))
+	req.Header.Set("Authorization", r.Header.Get("Channel"))
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
+}
+
+func withCORS(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Channel")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+	http.HandleFunc("/api/get", withCORS(ReverseProxyAPIGet))
+	http.HandleFunc("/api/login", withCORS(ReverseProxyAPILogin))
+	http.HandleFunc("/api/post", withCORS(ReverseProxyAPIPost))
+	http.ListenAndServe(":8080", nil)
+}
